50e4ef47c34d8a0767c41144024d8e918845bc33
Ek Ae <ekzin15@gmail.com>
2023-02-11 19:27:53 +0700

    add verified user, session, and error message

--- login.php
+++ login.php
@@ -1,7 +1,25 @@
 <?php
-    if(isset($_POST["submit"])) {
-        echo "Form was submitted";
+  require_once("./includes/classes/FormSanitizer.php");
+  require_once("./includes/classes/Constants.php");
+  require_once("./includes/classes/Account.php");
+  require_once("./includes/config.php");
+  require_once("./includes/classes/Utils.php");
+
+  $account = new Account($con);
+  
+  if(isset($_POST["submit"])) {
+    $username = FormSanitizer::sanitizeFormUsername($_POST["username"]);
+    $password = FormSanitizer::sanitizeFormPassword($_POST["password"]);
+
+    $success = $account->login($username, $password);
+
+    if($success) {
+      $_SESSION["userLoggedIn"] = $username;
+      header("Location: index.php");
     }
+ }
+
+
 ?>
 <!DOCTYPE html>
 <html>
@@ -27,8 +45,9 @@
       </div>
 
       <form method="POST">
-
-        <input type="text" name="username" placeholder="Username" required>
+        <?php echo $account->getError(Constants::$login_failed) ?>
+        <input type="text" name="username" placeholder="Username" value="<?php Utils::getInputValue('username') ?>"
+          required>
 
         <input type="password" name="password" placeholder="Password" required>
 
