b8d4e221f6e18c93116f30f0e40a823ea3e69591
Ek Ae <ekzin15@gmail.com>
2023-02-11 19:25:27 +0700

    constants fielts class

---
+++ includes/classes/Constants.php
@@ -0,0 +1,16 @@
+<?php 
+
+class Constants {
+  public static $first_name_characters = "Your first name must be between 2 and 25 characters.";
+  public static $last_name_characters = "Your last name must be between 2 and 25 characters.";
+  public static $username_characters = "Your username must be between 5 and 25 characters.";
+  public static $username_taken = "Your username has already been taken.";
+  public static $email_doesnt_match = "Your email address does not match.";
+  public static $email_invalid = "Your email address is invalid.";
+  public static $email_taken = "Your email address has already been taken.";
+  public static $password_doesnt_match = "Your password does not match.";
+  public static $password_characters = "Your password must be between 8 and 25 characters.";
+  public static $login_failed = "Your password or email address is not correct.";
+  
+  
+}
